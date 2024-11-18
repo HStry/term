7a633ee3555a19a7896a2a6a088be9c4c981dc06
Hans Strijker <45356184+HStry@users.noreply.github.com>
2024-11-18 13:51:25 +0100

    Update sysutils.sh

--- src/term/bash/hello-interactive.d/sysutils.sh
+++ src/term/bash/hello-interactive.d/sysutils.sh
@@ -124,4 +124,28 @@ qdig() {
   dig $@ | grep -v '^\s*\(;\|$\)'
 }
 
-export -f to_bool color colorize hosts_reachable hosts_reachable_wait qdig
+sys.findcmd() {
+  [[ -z "${1}" ]] && echo "Missing command argument." >&2 && return 1
+  local IFS=$'\n'
+  local path paths=( $(for path in $(echo "${PATH}" | tr ':' '\n'); do
+                         path="$(realpath "${path}" 2> /dev/null)"
+                         [[ -d "${path}" ]] && echo "${path}"
+                       done | sort -u) )
+  local name
+  for name in "$@"; do
+    find "${paths[@]}" -maxdepth 1 -executable -iname "${name}"
+  done | sort -u
+}
+
+# With regards to sys.findcmd; wanted to do a single run, but couldn't
+# get the code below to work, fucking globbing gets in the way.
+#   local name names=()
+#   for name in "$@"; do
+#     names+=( -or )
+#     names+=( -iname )
+#     names+=( $(echo "${name}" | sed -e 's/\*/\\\*/') )
+#   done
+#   unset names[0]
+#   find "${paths[@]}" -maxdepth 1 -executable "${names[@]}"
+
+export -f to_bool color colorize hosts_reachable hosts_reachable_wait qdig sys.findcmd
